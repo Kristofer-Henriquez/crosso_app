1d68b6c0805eaf8053642707e3834bf6de9d0555
Kristofer <kristoferhenriquez@gmail.com>
2020-08-30 10:26:18 -0500

    created update action for combo model

--- app/controllers/api/combos_controller.rb
+++ app/controllers/api/combos_controller.rb
@@ -20,4 +20,16 @@ class Api::CombosController < ApplicationController
     end
   end
 
+  def update
+    @combo = Combo.find_by(id: params[:id])
+    @combo.combo_video = params[:combo_video] || @combo.combo_video
+    @combo.character_id = params[:character_id] || @combo.character_id
+    @combo.notation = params[:notation] || @combo.notation
+    @combo.user_id = params[:user_id] || @combo.user_id
+
+    @combo.save!
+    render "show.json.jb"
+  end
+
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,10 +5,10 @@ Rails.application.routes.draw do
   # EXAMPLE JSON ROUTE WITH API NAMESPACE
   namespace :api do
     get "/games" => "games#index"
-    get "games/:id" => "games#show"
+    get "/games/:id" => "games#show"
 
-    get "combos/:id" => "combos#show"
-    post "combos" => "combos#create"
-    
+    get "/combos/:id" => "combos#show"
+    post "/combos" => "combos#create"
+    patch "/combos/:id" => "combos#update"
   end
 end
